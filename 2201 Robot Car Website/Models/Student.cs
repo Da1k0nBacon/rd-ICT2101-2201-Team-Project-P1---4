b4705f026a218d77c581c83be05c65639f0a86b1
Da1k0nBacon <decoyemail002@gmail.com>
2021-12-02 03:29:35 +0800

    Connect to DB and extract from student table
    Take note in your DataAccess.cs change password to the password you set on your local databse

--- 2201 Robot Car Website/Models/Student.cs
+++ 2201 Robot Car Website/Models/Student.cs
@@ -7,14 +7,10 @@ using System.Web;
 namespace _2201_Robot_Car_Website.Models
 
 {
-    public partial class Student
+    public class Student
     {
-        public int Sid { get; set; }
-
-        [MaxLength(45)]
         public string StudentName { get; set; }
 
-        [MaxLength(4)]
         public string Class { get; set; }
 
     }
