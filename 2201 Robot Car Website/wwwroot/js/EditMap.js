05e23a805f9e436eb3250533ebfba77bbb234483
nicholaslum97 <2001605@sit.singaporetech.edu.sg>
2021-12-05 14:15:29 +0800

    threw the script in html into a js file then linked it to html

---
+++ 2201 Robot Car Website/wwwroot/js/EditMap.js
@@ -0,0 +1,27 @@
+﻿var value = 0
+function increment(thisID) {
+    value = parseInt(document.getElementById(thisID).value) + 1;
+    if (value == 4) {
+        value = 0;
+    }
+    if (value == 0) {
+        document.getElementById(thisID).style.backgroundColor = "white";
+    }
+    else if (value == 1) {
+        document.getElementById(thisID).style.backgroundColor = "red";
+    }
+    else if (value == 2) {
+        document.getElementById(thisID).style.backgroundColor = "green";
+    }
+    else if (value == 3) {
+        document.getElementById(thisID).style.backgroundColor = "yellow";
+    }
+    document.getElementById(thisID).value = value.toString();
+}
+function submit() {
+    var map = new Array();
+    for (var i = 1; i < 16; i++) {
+        map[i - 1] = document.getElementById(i.toString()).value;
+    }
+    alert(map);
+}
