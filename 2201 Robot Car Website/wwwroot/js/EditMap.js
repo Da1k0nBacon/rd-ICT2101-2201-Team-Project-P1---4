be676f8b0212e18668e61586353a17d5a533c627
nicholaslum97 <2001605@sit.singaporetech.edu.sg>
2021-12-06 01:22:41 +0800

    Updated edit map stuff

--- 2201 Robot Car Website/wwwroot/js/EditMap.js
+++ 2201 Robot Car Website/wwwroot/js/EditMap.js
@@ -19,9 +19,41 @@ function increment(thisID) {
     document.getElementById(thisID).value = value.toString();
 }
 function submit() {
-    var map = new Array();
-    for (var i = 1; i < 16; i++) {
-        map[i - 1] = document.getElementById(i.toString()).value;
-    }
-    alert(map);
+    var jsonList = [];
+    jsonList.push({
+        'Grid1': document.getElementById("1").value,
+        'Grid2': document.getElementById("2").value,
+        'Grid3': document.getElementById("3").value,
+        'Grid4': document.getElementById("4").value,
+        'Grid5': document.getElementById("5").value,
+        'Grid6': document.getElementById("6").value,
+        'Grid7': document.getElementById("7").value,
+        'Grid8': document.getElementById("8").value,
+        'Grid9': document.getElementById("9").value,
+        'Grid10': document.getElementById("10").value,
+        'Grid11': document.getElementById("11").value,
+        'Grid12': document.getElementById("12").value,
+        'Grid13': document.getElementById("13").value,
+        'Grid14': document.getElementById("14").value,
+        'Grid15': document.getElementById("15").value,
+        'Grid16': document.getElementById("16").value,
+    })
+    const jsonString = JSON.stringify(jsonList);
+
+    console.log(jsonString);
+
+    $.ajax({
+        type: "POST",
+        url: 'SendMapData',
+        dataType: "json",
+        data: { 'mapData': jsonString },
+        cache: false,
+        success: function (data) {
+            alert("Saved to DB");
+        },
+        failure: function (data) {
+            alert("Cannot ah sial");
+        }
+    });
+
 }
