10ec4c67323eb26564493e69fbcb8e8f97cfface
SynRein <2002170@sit.singaporetech.edu.sg>
2021-12-06 14:12:39 +0800

    updates

---
+++ 2201 Robot Car Website/Controllers/Login.cs
@@ -0,0 +1,33 @@
+﻿using _2201_Robot_Car_Website.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using _2201_Robot_Car_Website.Data;
+
+namespace _2201_Robot_Car_Website.Controllers
+{
+    public class Login : Controller
+    {
+        private readonly ILogger<Login> _logger;
+
+        public Login(ILogger<Login> logger)
+        {
+            _logger = logger;
+        }
+        [HttpGet]
+        public IActionResult LoginMode()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult LoginMode(Student student)
+        {
+            //need edit
+            return RedirectToAction("Student", "Home");
+
+            return View();
+        }
+
+
+    }
+}
